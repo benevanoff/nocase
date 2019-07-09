47f1f70ed488e3e8852d27cd75c05cd5fbe6fb8a
Eraz <48658740+SirEraz@users.noreply.github.com>
2019-07-09 13:57:14 -0500

    Add ncLower function - convert to all lowercase

---
+++ nc.py
@@ -0,0 +1,61 @@
+#convert to all lower case
+def ncLower(string):
+	ncList = []
+	for char in string:
+		ncList.append(char)
+	for i in ncList:
+		if i = "A":
+			ncList[i] = "a"
+		elif i = "B":
+			ncList[i] = "b"
+		elif i = "C":
+			ncList[i] = "c"
+		elif i = "D":
+			ncList[i] = "d"
+		elif i = "E":
+			ncList[i] = "e"
+		elif i = "F":
+			ncList[i] = "F"
+		elif i = "G":
+			ncList[i] = "g"
+		elif i = "H":
+			ncList[i] = "h"
+		elif i = "I":
+			ncList[i] = "i"
+		elif i = "J":
+			ncList[i] = "j"
+		elif i = "K":
+			ncList[i] = "k"
+		elif i = "L":
+			ncList[i] = "l"
+		elif i = "M":
+			ncList[i] = "m"
+		elif i = "N":
+			ncList[i] = "n"
+		elif i = "O":
+			ncList[i] = "o"
+		elif i = "P":
+			ncList[i] = "p"
+		elif i = "Q":
+			ncList[i] = "q"
+		elif i = "R":
+			ncList[i] = "r"
+		elif i = "S":
+			ncList[i] = "s"
+		elif i = "T":
+			ncList[i] = "t"
+		elif i = "U":
+			ncList[i] = "u"
+		elif i = "V":
+			ncList[i] = "v"
+		elif i = "W":
+			ncList[i] = "w"
+		elif i = "X":
+			ncList[i] = "x"
+		elif i = "Y":
+			ncList[i] = "y"
+		elif i = "Z":
+			ncList[i] = "z"
+		else:
+			continue
+	string = str(''.join(string))
