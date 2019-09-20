0fa0f11342d1bc2ebb571d1488c09effae7cc3e6
benevanoff <55565574+benevanoff@users.noreply.github.com>
2019-09-19 17:55:46 -0700

    use ascii conversions with addition/subtraction
    instead of a millions if/elseif statements

--- nc.py
+++ nc.py
@@ -1,123 +1,31 @@
-#convert to all lower case
-def ncLower(string):
-	ncList = []
-	for char in string:
-		ncList.append(char)
-	for i in ncList:
-		if i = "A":
-			ncList[i] = "a"
-		elif i = "B":
-			ncList[i] = "b"
-		elif i = "C":
-			ncList[i] = "c"
-		elif i = "D":
-			ncList[i] = "d"
-		elif i = "E":
-			ncList[i] = "e"
-		elif i = "F":
-			ncList[i] = "f"
-		elif i = "G":
-			ncList[i] = "g"
-		elif i = "H":
-			ncList[i] = "h"
-		elif i = "I":
-			ncList[i] = "i"
-		elif i = "J":
-			ncList[i] = "j"
-		elif i = "K":
-			ncList[i] = "k"
-		elif i = "L":
-			ncList[i] = "l"
-		elif i = "M":
-			ncList[i] = "m"
-		elif i = "N":
-			ncList[i] = "n"
-		elif i = "O":
-			ncList[i] = "o"
-		elif i = "P":
-			ncList[i] = "p"
-		elif i = "Q":
-			ncList[i] = "q"
-		elif i = "R":
-			ncList[i] = "r"
-		elif i = "S":
-			ncList[i] = "s"
-		elif i = "T":
-			ncList[i] = "t"
-		elif i = "U":
-			ncList[i] = "u"
-		elif i = "V":
-			ncList[i] = "v"
-		elif i = "W":
-			ncList[i] = "w"
-		elif i = "X":
-			ncList[i] = "x"
-		elif i = "Y":
-			ncList[i] = "y"
-		elif i = "Z":
-			ncList[i] = "z"
-		else:
-			continue
-	string = str(''.join(string))
-	
 #convert to all upper case
+def ncLower(string):
+        ncList = []
+        for char in string:
+                ncList.append(char)
+        i = 0 # iterator
+        for x in ncList:
+                int = ord(x)
+                if (int > 64 and int < 91):
+                        ncList[i] = chr(int+32)
+                else:
+                        ncList[i] = ncList[i]
+                i = i + 1
+        string = str(''.join(ncList))
+        return string
+
+#convert to all lower case
 def ncUpper(string):
-	ncList = []
-	for char in string:
-		ncList.append(char)
-	for i in ncList:
-		if i = "a":
-			ncList[i] = "A"
-		elif i = "b":
-			ncList[i] = "B"
-		elif i = "c":
-			ncList[i] = "C"
-		elif i = "d":
-			ncList[i] = "D"
-		elif i = "e":
-			ncList[i] = "E"
-		elif i = "f":
-			ncList[i] = "F"
-		elif i = "g":
-			ncList[i] = "G"
-		elif i = "h":
-			ncList[i] = "H"
-		elif i = "i":
-			ncList[i] = "I"
-		elif i = "j":
-			ncList[i] = "J"
-		elif i = "k":
-			ncList[i] = "K"
-		elif i = "l":
-			ncList[i] = "L"
-		elif i = "m":
-			ncList[i] = "M"
-		elif i = "n":
-			ncList[i] = "N"
-		elif i = "o":
-			ncList[i] = "O"
-		elif i = "p":
-			ncList[i] = "P"
-		elif i = "q":
-			ncList[i] = "Q"
-		elif i = "r":
-			ncList[i] = "R"
-		elif i = "s":
-			ncList[i] = "S"
-		elif i = "t":
-			ncList[i] = "T"
-		elif i = "u":
-			ncList[i] = "U"
-		elif i = "v":
-			ncList[i] = "V"
-		elif i = "w":
-			ncList[i] = "W"
-		elif i = "x":
-			ncList[i] = "X"
-		elif i = "y":
-			ncList[i] = "Y"
-		elif i = "z":
-			ncList[i] = "Z"
-		else:
-			continue
-	string = str(''.join(string))
+        ncList = []
+        for char in string:
+                ncList.append(char)
+        i = 0 # iterator
+        for x in ncList:
+                int = ord(x)
+                if (int > 96 and int < 123):
+                        ncList[i] = chr(int-32)
+                else:
+                        ncList[i] = ncList[i]
+                i = i + 1
+        string = str(''.join(ncList))
+        return string
